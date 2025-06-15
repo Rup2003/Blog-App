ba36f48620b7b91990c3e412eb754e1f24488fc7
Rupak Saha <rupaksaha003@gmail.com>
2025-06-15 22:59:24 +0530

    minor update on controller

--- server/src/controller/blog.controller.js
+++ server/src/controller/blog.controller.js
@@ -3,23 +3,61 @@ import User from "../models/user.models.js";
 import { ApiError } from "../utils/api-error.js";
 import { asyncHandler } from "../utils/async-handler.js";
 import { ApiResponse } from "../utils/api-response.js";
+import Like from "../models/like.model.js";
 
-export const getAllBlogs = asyncHandler(async (req, res, next) => {});
+export const getAllBlogs = asyncHandler(async (req, res, next) => {
+  const userId = req.id;
 
-export const getFeedBlogs = asyncHandler(async (req, res, next) => {});
+  const user = await User.findById(userId);
 
-export const getBlogsByID = asyncHandler(async (req, res, next) => {});
+  if (!user) {
+    throw new ApiError(404, "No user with this userId exists");
+  }
+  const blogs = await Blog.findOne({});
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, blogs, `blogs successfully send`));
+});
+
+export const getFeedBlogs = asyncHandler(async (req, res, next) => {
+  const userId = req.id;
+
+  const user =  await User.findById(userId);
+
+  if (!user) {
+    throw new ApiError(404, "no user exists");
+  }
+
+
+  return res.status(200).json(new ApiResponse(200, {}, "Feed Blogs Fetched"));
+});
+
+export const getBlogsByID = asyncHandler(async (req, res, next) => {
+  const blogId = req.params.id;
+  const userId = req.id;
+
+  const user = await User.findById(userId);
+  const blog = await Blog.findById(blogId);
+  if (!user) {
+    throw new ApiError(404, "No user with this userId exists");
+  }
+  if (!blog) {
+    throw new ApiError(404, "Blog not found");
+  }
+  return res
+    .status(200)
+    .json(new ApiResponse(200, blog, `update blog successfully`));
+});
 
 export const createBlog = asyncHandler(async (req, res, next) => {
   const { title, description } = req.body;
   const userId = req.id;
 
-
   const user = await User.findById(userId);
   if (!user) {
     throw new ApiError(404, "No user with this userId exists");
   }
-
 
   if (!title || !description) {
     throw new ApiError(400, "All fields are required");
@@ -48,10 +86,71 @@ export const createBlog = asyncHandler(async (req, res, next) => {
 
 export const updateBlog = asyncHandler(async (req, res, next) => {});
 
-export const deleteBlog = asyncHandler(async (req, res, next) => {});
+export const deleteBlog = asyncHandler(async (req, res, next) => {
+  const blogId = req.params.id;
+  const userId = req.id;
 
-export const toggleLike = asyncHandler(async (req, res, next) => {});
+  const user = await User.findById(userId);
+  const blog = await Blog.findById(blogId);
+  if (!user) {
+    throw new ApiError(404, "No user with this userId exists");
+  }
+  if (!blog) {
+    throw new ApiError(404, "Blog not found");
+  }
+  await Blog.findByIdAndDelete(blogId);
 
-export const addComment = asyncHandler(async (req, res, next) => {});
+  return res
+    .status(200)
+    .json(new ApiResponse(200, blog, `blogs successfully deleted`));
+});
 
-export const getComment = asyncHandler(async (req, res, next) => {});
+export const toggleLike = asyncHandler(async (req, res, next) => {
+  const blogId = req.params.id;
+  const userId = req.id;
+
+  const blog = await Blog.findById(blogId);
+
+   const user = await User.findById(userId);
+
+  if (!blog) {
+    throw new ApiError(404, "Blog not found");
+  }
+
+  const existingLike = await Like.findOne({
+    user: userId,
+    target: "blog",
+    targetId: blogId,
+  });
+
+  let message;
+  if (existingLike) {
+    // To UnLike
+    await Like.deleteOne({
+      user: userId,
+      target: "blog",
+      targetId: blogId,
+    });
+
+    await blog.updateOne({
+      $pull: { likes: existingLike._id },
+    });
+
+    message = `${user.username} unliked the blog`;
+  } else {
+    // To Like
+    const newLike = await Like.create({
+      user: userId,
+      target: "blog",
+      targetId: blogId,
+    });
+
+    await blog.updateOne({
+      $push: { likes: newLike._id },
+    });
+    message = `${user.username} liked the blog`;
+  }
+
+  return res.status(200).json(new ApiResponse(200, null, message));
+});
+
