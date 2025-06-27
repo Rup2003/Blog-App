f5980c164657bbd6a8776ad508d715bc7d8d11bd
Rupak Saha <rupaksaha003@gmail.com>
2025-06-27 10:16:26 +0530

    minor changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import {Routes, Route} from "react-router"
+import { HomePage } from './pages/HomePage'
+import { RegisterPage } from './pages/RegisterPage'
+import { LoginPage } from './pages/LoginPage'
+import { ProfilePage } from './pages/ProfilePage'
 
 export const App = () => {
   return (
-    <div>App</div>
+    <Routes>
+      <Route path= "/" element={<HomePage/>} />
+      <Route path= "/profile" element={<ProfilePage/>} />
+      <Route path= "/blogCreation" element={<h1>create blog</h1>} />
+      <Route path= "/login" element={<LoginPage/>} />
+      <Route path= "/register" element={<RegisterPage/>} />
+    </Routes>
   )
 }
